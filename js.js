ae10411682243a085558fe83d2aeeb31fa9840e3
jonr1gg1ns <jonathanriggins08@gmail.com>
2024-12-15 21:46:50 -0800

    added html and css, created the base calc no js yet

---
+++ js.js
@@ -0,0 +1,17 @@
+let buttonOne = document.querySelector(".\\1");
+let buttonTwo = document.querySelector(".\\2");
+let buttonThree = document.querySelector(".\\3");
+let buttonFour = document.querySelector(".\\4");
+let buttonFive = document.querySelector(".\\5");
+let buttonSix = document.querySelector(".\\6");
+let buttonSeven = document.querySelector(".\\7");
+let buttonEight = document.querySelector(".\\8");
+let buttonNine = document.querySelector(".\\9");
+let buttonZero = document.querySelector(".\\0");
+let buttonClear = document.querySelector(".clear");
+let buttonEqual = document.querySelector(".equal");
+let buttonDivide = document.querySelector(".divide");
+let buttonAdd = document.querySelector(".plus");
+let buttonMultiply = document.querySelector(".multiply");
+let buttonSubtract = document.querySelector(".subtract");
+
